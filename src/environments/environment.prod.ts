0c7e199772de14fbc0ee9dcdddf3e4c1bd475afd
renzopalmieri <renzopalmieri@gmail.com>
2019-02-25 13:35:30 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   firebase : {
-    apiKey: 'AIzaSyBaPiW4ctqx5FFRH2w1W4c71GJXa2sMZsM',
+    apiKey: '',
     authDomain: 'myfilme-57a3f.firebaseapp.com',
     databaseURL: 'https://myfilme-57a3f.firebaseio.com',
     projectId: 'myfilme-57a3f',
